04da359309ad613bc558a47098d718023d957fb4
C Bendix <christopherbendix@gmail.com>
2018-10-16 14:14:27 -0700

    Add files via upload
    Exercise 1.7
    Write a complete Java program called Mantra that prints the following output. Use at least one static method besides main.
    
    There's one thing every coder must understand:
    The System.out.println command.
    
    There's one thing every coder must understand:
    The System.out.println command.

---
+++ Mantra.java
@@ -0,0 +1,15 @@
+
+public class Mantra {
+
+	public static void main(String[] args) {
+		chant();
+		System.out.println();
+		chant();
+		
+	}
+
+	public static void chant() {
+		System.out.println("There's one thing every code must understand:");
+		System.out.println("The System.out.println command.");
+	}
+}
