6dca230ab6997c7c76e171080fc34ae42f52a28a
C Bendix <christopherbendix@gmail.com>
2018-10-16 12:03:12 -0700

    Add files via upload
    Exercise 1.2
    Write a complete Java program called Spikey that prints the following output:
      \/
     \\//
    \\\///
    ///\\\
     //\\
      /\

---
+++ Spikey.java
@@ -0,0 +1,17 @@
+
+public class Spikey {
+
+	public static void main(String[] args) {
+		spikePattern();
+	}
+
+	
+	public static void spikePattern() {
+		System.out.println("  \\/");
+		System.out.println(" \\\\//");
+		System.out.println("\\\\\\///");
+		System.out.println("///\\\\\\");
+		System.out.println(" //\\\\");
+		System.out.println("  /\\");
+	}
+}
