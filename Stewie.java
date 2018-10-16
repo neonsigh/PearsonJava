16b0afe69c35c010786c6bfd1343d43aa01d965b
C Bendix <christopherbendix@gmail.com>
2018-10-16 11:57:34 -0700

    Add files via upload
    Write a complete Java program called Stewie that prints the following output:
    //////////////////////
    || Victory is mine! ||
    \\\\\\\\\\\\\\\\\\\\\\

---
+++ Stewie.java
@@ -0,0 +1,14 @@
+
+public class Stewie {
+
+	public static void main(String[] args) {
+		victory();
+	}
+
+	
+	public static void victory() {
+		System.out.println("//////////////////////");
+		System.out.println("|| Victory is mine! ||");
+		System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+	}
+}
