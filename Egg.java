161ca33eae26f81cd50858e497cfa72cb6220b0b
C Bendix <christopherbendix@gmail.com>
2018-10-16 17:21:09 -0700

    Add files via upload
    Exercise 1.9
    Write a program called Egg that displays the following output:
    
      _______
     /       \
    /         \
    -\"-'-\"-'-\"-
    \         /
    \_______/

---
+++ Egg.java
@@ -0,0 +1,15 @@
+public class Egg {
+
+	public static void main(String[] args) {
+		writeEgg();
+	}
+	
+	public static void writeEgg() {
+		System.out.println("  _______");
+		System.out.println(" /       \\");
+		System.out.println("/         \\");
+		System.out.println("-\"-'-\"-'-\"-");
+		System.out.println("\\         /");
+		System.out.println(" \\_______/");
+	}
+}
