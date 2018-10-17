b17d310fe063a20ca6f2f31b3b379bb9de58f773
C Bendix <christopherbendix@gmail.com>
2018-10-16 17:30:45 -0700

    Add files via upload
    Exercise 1.10
    Modify the program from the previous exercise to become a new program Egg2 that displays the following output. Use static methods as appropriate.
    
      _______
     /       \
    /         \
    \         /
    \_______/
    
    -\"-'-\"-'-\"-
    
      _______
     /       \
    /         \
    \         /
    \_______/

---
+++ Egg2.java
@@ -0,0 +1,18 @@
+public class Egg2 {
+
+	public static void main(String[] args) {
+		writeEgg();
+		System.out.println();
+		System.out.println("-\"-'-\"-'-\"-");
+		System.out.println();
+		writeEgg();
+	}
+	
+	public static void writeEgg() {
+		System.out.println("  _______");
+		System.out.println(" /       \\");
+		System.out.println("/         \\");
+		System.out.println("\\         /");
+		System.out.println(" \\_______/");
+	}
+}
